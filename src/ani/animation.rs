8ea2a627c57a7c067b2a152713ffb25fcd52bac2
mgi388 <135186256+mgi388@users.noreply.github.com>
2025-08-19 16:03:29 +1000

    add bevy_spritesheet_animation feature

--- src/ani/animation.rs
+++ src/ani/animation.rs
@@ -12,6 +12,34 @@ pub enum AnimationDuration {
     PerRepetition(u32),
 }
 
+#[cfg(feature = "bevy_spritesheet_animation")]
+impl From<bevy_spritesheet_animation::animation::AnimationDuration> for AnimationDuration {
+    fn from(duration: bevy_spritesheet_animation::animation::AnimationDuration) -> Self {
+        match duration {
+            bevy_spritesheet_animation::animation::AnimationDuration::PerFrame(duration) => {
+                AnimationDuration::PerFrame(duration)
+            }
+            bevy_spritesheet_animation::animation::AnimationDuration::PerRepetition(duration) => {
+                AnimationDuration::PerRepetition(duration)
+            }
+        }
+    }
+}
+
+#[cfg(feature = "bevy_spritesheet_animation")]
+impl From<AnimationDuration> for bevy_spritesheet_animation::animation::AnimationDuration {
+    fn from(duration: AnimationDuration) -> Self {
+        match duration {
+            AnimationDuration::PerFrame(duration) => {
+                bevy_spritesheet_animation::animation::AnimationDuration::PerFrame(duration)
+            }
+            AnimationDuration::PerRepetition(duration) => {
+                bevy_spritesheet_animation::animation::AnimationDuration::PerRepetition(duration)
+            }
+        }
+    }
+}
+
 /// The repeat behavior of an animation.
 #[derive(Debug, Clone, Copy, Default, PartialEq, Eq, Hash, Reflect)]
 #[cfg_attr(feature = "serde", derive(Deserialize, Serialize))]
@@ -21,6 +49,30 @@ pub enum AnimationRepeat {
     #[default]
     Loop,
     Times(usize),
+}
+
+#[cfg(feature = "bevy_spritesheet_animation")]
+impl From<bevy_spritesheet_animation::animation::AnimationRepeat> for AnimationRepeat {
+    fn from(repeat: bevy_spritesheet_animation::animation::AnimationRepeat) -> Self {
+        match repeat {
+            bevy_spritesheet_animation::animation::AnimationRepeat::Loop => AnimationRepeat::Loop,
+            bevy_spritesheet_animation::animation::AnimationRepeat::Times(times) => {
+                AnimationRepeat::Times(times)
+            }
+        }
+    }
+}
+
+#[cfg(feature = "bevy_spritesheet_animation")]
+impl From<AnimationRepeat> for bevy_spritesheet_animation::animation::AnimationRepeat {
+    fn from(repeat: AnimationRepeat) -> Self {
+        match repeat {
+            AnimationRepeat::Loop => bevy_spritesheet_animation::animation::AnimationRepeat::Loop,
+            AnimationRepeat::Times(times) => {
+                bevy_spritesheet_animation::animation::AnimationRepeat::Times(times)
+            }
+        }
+    }
 }
 
 /// The direction of an animation or clip.
@@ -33,6 +85,40 @@ pub enum AnimationDirection {
     Forwards,
     Backwards,
     PingPong,
+}
+
+#[cfg(feature = "bevy_spritesheet_animation")]
+impl From<bevy_spritesheet_animation::animation::AnimationDirection> for AnimationDirection {
+    fn from(direction: bevy_spritesheet_animation::animation::AnimationDirection) -> Self {
+        match direction {
+            bevy_spritesheet_animation::animation::AnimationDirection::Forwards => {
+                AnimationDirection::Forwards
+            }
+            bevy_spritesheet_animation::animation::AnimationDirection::Backwards => {
+                AnimationDirection::Backwards
+            }
+            bevy_spritesheet_animation::animation::AnimationDirection::PingPong => {
+                AnimationDirection::PingPong
+            }
+        }
+    }
+}
+
+#[cfg(feature = "bevy_spritesheet_animation")]
+impl From<AnimationDirection> for bevy_spritesheet_animation::animation::AnimationDirection {
+    fn from(direction: AnimationDirection) -> Self {
+        match direction {
+            AnimationDirection::Forwards => {
+                bevy_spritesheet_animation::animation::AnimationDirection::Forwards
+            }
+            AnimationDirection::Backwards => {
+                bevy_spritesheet_animation::animation::AnimationDirection::Backwards
+            }
+            AnimationDirection::PingPong => {
+                bevy_spritesheet_animation::animation::AnimationDirection::PingPong
+            }
+        }
+    }
 }
 
 /// A clip in an animation.
